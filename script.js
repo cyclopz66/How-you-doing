acf5a070535a00114bb57480f4f71e84a3ae1944
cyclopz66 <gustavomartinspinheiro@gmail.com>
2021-05-23 04:39:38 -0300

    HAHAGAGAGA

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ function foo() {
   let text = document.querySelector("h1");
   let button = document.querySelector("button");
   document.body.style.backgroundImage = "url(data/image/Joey.gif)";
+  document.body.style.height = "1000000px";
   text.classList.add("hidden");
   button.classList.add("hidden");
 }
