0e14a20bb6689edf3825c725686960519f919451
cyclopz66 <gustavomartinspinheiro@gmail.com>
2021-05-23 04:18:37 -0300

    HAHAHA

---
+++ script.js
@@ -0,0 +1,7 @@
+function foo() {
+  let text = document.querySelector("h1");
+  let button = document.querySelector("button");
+  document.body.style.backgroundImage = "url(data/image/Joey.gif)";
+  text.classList.add("hidden");
+  button.classList.add("hidden");
+}
